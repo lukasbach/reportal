be6fd61d99d4e7c2da5378157f0fbf5cf19a60d6
Lukas Bach <contact@lukasbach.com>
2023-04-16 13:53:15 +0200

    todo

--- src/components/filter-list/use-calc-page-size.ts
+++ src/components/filter-list/use-calc-page-size.ts
@@ -5,6 +5,7 @@ export const useCalcPageSize = <T extends Element>(itemHeight: number) => {
   const [measures, ref] = useMeasure<T>();
 
   const pageSize = useMemo(() => {
+    // TODO FIX: dont use the tbody, but the div above the table element. Make sure that only the table is mounted in that. Subtract a fixed table header height.
     return Math.floor((measures?.height ?? itemHeight) / itemHeight) - 1;
   }, [itemHeight, measures?.height]);
 

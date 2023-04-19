44a2773e4f838fd20d751b88e6c17e2eb7a06227
Lukas Bach <contact@lukasbach.com>
2023-04-19 23:00:13 +0200

    enable caching

--- src/firebase/db.ts
+++ src/firebase/db.ts
@@ -1,10 +1,19 @@
-import { collection, doc, DocumentReference, getFirestore } from "firebase/firestore";
-import { CollectionReference } from "@firebase/firestore";
+import {
+  collection,
+  doc,
+  DocumentReference,
+  initializeFirestore,
+  persistentLocalCache,
+  persistentMultipleTabManager,
+  CollectionReference,
+} from "firebase/firestore";
 import { FilterListStateEntry } from "../components/filter-list/types";
 import { firebaseApp } from "../firebase-app";
 import { DashboardConfigEntry } from "../common/widgets/types";
 
-export const db = getFirestore(firebaseApp);
+export const db = initializeFirestore(firebaseApp, {
+  localCache: persistentLocalCache({ tabManager: persistentMultipleTabManager() }),
+});
 
 export const listCollection = collection(db, "lists") as CollectionReference<FilterListStateEntry>;
 
